eb1ec895698a371ab196a4f45033f95d4819785a
DESKTOP-II130FV\DIGAICIA_BADRILO <artem.dehtiarov@nure.ua>
2021-05-03 18:06:54 +0300

    Created class ListWithHeapsort with properties and constructors

---
+++ Lab3/Lab3/ListWithHeapsort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class ListWithHeapsort
+    {
+        private int[] arr;
+        public int Len { get; }
+
+        public ListWithHeapsort() 
+            : this(Array.Empty<int>()) { }
+        public ListWithHeapsort(int item) 
+            : this(items: item){ }
+        public ListWithHeapsort(params int[] items)
+        {
+            this.arr = new int[items.Length < 5 ? 10 : items.Length * 2];
+            this.Len = items.Length;
+
+            for (int i = 0; i < Len; i++)
+            {
+                this.arr[i] = items[i];
+            }
+        }
+    }
+}
